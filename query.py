0ef97afc046040041d346393ceb62fa9aa50947a
Ashley Szelag <71200923+aeSzelag@users.noreply.github.com>
2024-03-18 20:55:50 -0400

    Create Menu: established the menu with quit feature

--- query.py
+++ query.py
@@ -1,3 +1,15 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+
+def menu():
+    print("Menu:")
+    print("Enter q for quit")
+
+if __name__ == "__main__":
+    menu()
+    user=input()
+    while user != "q":
+        menu()
+        user =input()
