7e96031d8ded6900155367cca452fd57e7cd4935
Ashley Szelag <71200923+aeSzelag@users.noreply.github.com>
2024-03-21 14:58:04 -0400

    added new list option to the menu

--- query.py
+++ query.py
@@ -1,3 +1,41 @@
 from movies import Movies
+import sys
 
 movies = Movies('./movies.txt')
+
+
+
+
+
+
+
+
+        
+
+
+#set up the menu options: 
+def menu_options():
+    print("Please choose an option:")
+    print("\'list\' to display a list of movies in the database")
+    print("\'q\' for exit")
+
+#method for displaying the movie dictionary
+def list_movies():
+    print("here")
+
+#process user input through the menu system
+def menu_selection():
+    while True:
+        menu_options()
+        option = input()
+
+        if option == 'q':
+            break
+        elif option == 'list':
+            list_movies()
+        else:
+            print("invalid option")
+
+
+if __name__ == "__main__":
+    menu_selection()
